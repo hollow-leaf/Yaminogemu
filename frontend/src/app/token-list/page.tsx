e6889d6710692264677ecaad3b7143063324d8c4
crypto0627 <jake0627a1@gmail.com>
2024-12-12 17:33:46 +0800

    feat: defi & token-list

--- frontend/src/app/token-list/page.tsx
+++ frontend/src/app/token-list/page.tsx
@@ -1,10 +1,33 @@
 'use client'
+import { useRouter } from 'next/navigation'
 import TokenRankings from './token-list'
-import { useWallet } from '@solana/wallet-adapter-react'
+import { useDynamicContext, useIsLoggedIn } from '@dynamic-labs/sdk-react-core'
+import { useEffect, useState } from 'react'
+import { isSolanaWallet } from '@dynamic-labs/solana-core'
+import { PublicKey } from '@solana/web3.js'
 
 export default function Home() {
-  const wallet = useWallet()
-  const isLoggedIn = wallet.connected
+  const router = useRouter()
+  const isLoggedIn = useIsLoggedIn()
+  const { primaryWallet } = useDynamicContext();
+  const [userAddr, setUserAddr] = useState<string | null>(null)
+
+  useEffect(() => {
+    if(isLoggedIn) {
+      if(primaryWallet != null) {
+        if(!isSolanaWallet(primaryWallet)) {
+          router.replace('/')
+        }
+        const fromKey = new PublicKey(primaryWallet.address); 
+        setUserAddr(fromKey.toBase58())
+      } else {
+        router.replace('/')
+      }
+      router.replace('/token-list')
+    } else {
+      router.replace('/')
+    }
+  }, [])
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       {isLoggedIn && <TokenRankings />}
